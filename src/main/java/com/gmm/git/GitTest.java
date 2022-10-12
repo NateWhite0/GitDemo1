b42b2e136ec38d04b9fd9547ca9eaa307c63d47f
Frank <Frank@Ming.com>
2022-10-12 17:32:40 +0800

    push commit

--- src/main/java/com/gmm/git/GitTest.java
+++ src/main/java/com/gmm/git/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
         System.out.println("Hello hot fix");
         System.out.println("test");
         System.out.println("fix-commit");
+        System.out.println("OnlyYou123");
+
     }
 
 }
